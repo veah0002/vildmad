22680569e6d54900d7a3fe47239aad63b831251a
Cecilie <ceka0002@stud.kea.dk>
2024-02-16 12:52:39 +0100

    navn skift

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,32 +1,36 @@
 //https://oqxpqyntqutkvwkzxvju.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs
 
-fetch("https://oqxpqyntqutkvwkzxvju.supabase.co/rest/v1/vild_mad",{
-    method:"GET",
-    headers:{
-        apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs"
-    }
+fetch("https://oqxpqyntqutkvwkzxvju.supabase.co/rest/v1/vild_mad", {
+  method: "GET",
+  headers: {
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs",
+  },
 })
-.then(res=>res.json())
-.then(showData)
+  .then((res) => res.json())
+  .then(showData);
 
-function showData(items){
-    //looper og kalder showData
-     items.forEach(showData);
+function showData(items) {
+  //looper og kalder showData
+  console.log("hvad er item ssssss: ", items);
+
+  items.forEach(showDataSingle);
 }
 
-function showData (item){
-console.log(item)}
+function showDataSingle(item) {
+  console.log(item);
 
-//fange template
-const template=document.querySelector("#mushroomTemplate").content;
+  //fange template
+  const template = document.querySelector("#mushroomTemplate").content;
 
-//Lave ne kopi
-const copy = template.cloneNode(true);
+  //Lave ne kopi
+  const copy = template.cloneNode(true);
 
-//Ændre indhold
-copy.querySelector("h3").textContent=item.name
-// copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  //Ændre indhold
+  copy.querySelector("h3").textContent = item.name;
+  copy.querySelector("img").src = `./img/${item.image}`;
 
-//Appende
-document.querySelector(".grid").appendChild(copy);
+  //Appende
+  document.querySelector(".grid").appendChild(copy);
+}
