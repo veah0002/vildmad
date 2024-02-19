6c6bad7decaca1ed149f986508b5a25bf4bc591f
Laura <laja0003@stud.kea.dk>
2024-02-19 14:55:54 +0100

    javascript produkt

--- js/produkt.js
+++ js/produkt.js
@@ -1,20 +1,27 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-fetch("https://oqxpqyntqutkvwkzxvju.supabase.co/rest/v1/vild_mad?id=" + id)
+console.log("Her kommer id", id);
+
+fetch("https://oqxpqyntqutkvwkzxvju.supabase.co/rest/v1/vild_mad?id=eq." + id, {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs",
+  },
+})
   .then((response) => response.json())
-  .then((data) => showProduct(data));
+  .then((data) => showItem(data));
 
 function showItem(item) {
-  console.log(item);
+  console.log("min item er ", item);
 
-  document.querySelector(".produkt-navn").textContent = item.name;
-  document.querySelector(".id1").textContent = item.id;
-  document.querySelector(".season1").textContent = item.season;
-  document.querySelector(".harvesting").textContent = item.foraging;
-  document.querySelector(".location1").textContent = item.location;
-  document.querySelector(".season").textContent = item.month;
-  document.querySelector(".beskrivelse").textContent = item.description;
+  document.querySelector(".produkt-navn").textContent = item[0].name;
+  //   document.querySelector(".id1").textContent = item[0].id;
+  document.querySelector(".season1").textContent = item[0].season;
+  document.querySelector(".harvesting").textContent = item[0].foraging;
+  document.querySelector(".location1").textContent = item[0].location;
+  document.querySelector(".season").textContent = item[0].month;
+  document.querySelector(".beskrivelse").textContent = item[0].description;
 
-  document.querySelector("img").src = `./img/${item.image}`;
+  document.querySelector("img").src = item[0].image;
 }
--- js/produktliste.js
+++ js/produktliste.js
@@ -4,8 +4,7 @@
 fetch("https://oqxpqyntqutkvwkzxvju.supabase.co/rest/v1/vild_mad", {
   method: "GET",
   headers: {
-    apikey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeHBxeW50cXV0a3Z3a3p4dmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MjkzNTAsImV4cCI6MjAyMzQwNTM1MH0.vKE6NAOQc3KfnX6D2uEpMzA7YrKmdbvnbv7deknPEPs",
   },
 })
   .then((res) => res.json())
@@ -29,6 +28,7 @@ function showDataSingle(item) {
 
   //Ændre indhold
   copy.querySelector("h3").textContent = item.name;
+  copy.querySelector(".klikVidere").href = `produkt.html?id=${item.id}`;
   copy.querySelector("img").src = `./img/${item.image}`;
 
   //Appende
